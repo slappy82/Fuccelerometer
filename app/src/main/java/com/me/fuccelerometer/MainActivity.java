d4caae9a40f75ea2dfc4055fd105365e8cd4977a
Campbell Maxwell <campbell.maxwell@enlighten.co.nz>
2025-03-25 04:19:06 +1300

    Initial commit, imported basic gyro and accel code from previous project

---
+++ app/src/main/java/com/me/fuccelerometer/MainActivity.java
@@ -0,0 +1,165 @@
+package com.me.fuccelerometer;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.me.fuccelerometer.databinding.ActivityMainBinding;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private Sensor gyro;
+
+    public Coordinates accelCoordinatesCurrent;
+    public Coordinates gyroCoordinatesCurrent;
+    //private Coordinates accelCoordinatesPrevious;
+    //private Coordinates gyroCoordinatesPrevious;
+    private AppBarConfiguration appBarConfiguration;
+
+    //private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Gyro", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Initialise iterative gyroscope and accelerometer objects
+        gyroCoordinatesCurrent = new Coordinates(0, 0, 0);
+        accelCoordinatesCurrent = new Coordinates(0, 0, 0);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        sensorManager.registerListener(sensorEventListener, accel, 50000);
+        sensorManager.registerListener(sensorEventListener, gyro, 50000);
+    }
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_GYROSCOPE: {
+                    gyroCoordinatesCurrent.SetAll(
+                            event.values[0],
+                            event.values[1],
+                            event.values[2]);
+                    break;
+                }
+                case Sensor.TYPE_ACCELEROMETER: {
+                    accelCoordinatesCurrent.SetAll(
+                            filterGravity(event.values[0], accelCoordinatesCurrent.X()),
+                            filterGravity(event.values[1], accelCoordinatesCurrent.Y()),
+                            filterGravity(event.values[2], accelCoordinatesCurrent.Z()));
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+    private double filterGravity(double newValue, double prevValue) {
+        final double ALPHA = 0.5;
+        return prevValue * ALPHA + (1 - ALPHA) * newValue;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(sensorEventListener, accel);
+        sensorManager.unregisterListener(sensorEventListener, gyro);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
