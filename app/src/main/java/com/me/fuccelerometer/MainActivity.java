dc9b4363b81632fd61aeedc4953f4dd90111ac08
Campbell Maxwell <cammaxwell2020@gmail.com>
2025-03-26 02:58:28 +1300

    Version updates

--- app/src/main/java/com/me/fuccelerometer/MainActivity.java
+++ app/src/main/java/com/me/fuccelerometer/MainActivity.java
@@ -26,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
     public Coordinates gyroCoordinatesCurrent;
     private AppBarConfiguration appBarConfiguration;
 
-    //private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
